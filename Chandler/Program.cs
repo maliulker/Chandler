e2e3c6967a8148df9b60b380fa4351e815b8a6d8
maliulker <maliulker2@gmail.com>
2024-08-15 22:33:56 +0300

    create first bot

--- Chandler/Program.cs
+++ Chandler/Program.cs
@@ -1,11 +1,30 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
 
 namespace Chandler;
 
-class Program
+public class Program
 {
-    static void Main(string[] args)
+    private static DiscordSocketClient _socketClient;
+    public static async Task Main()
     {
-        Console.WriteLine("Hello Chandler");
+        _socketClient = new DiscordSocketClient();
+        _socketClient.Log += Log; 
+
+        var token = File.ReadAllText(".env");
+
+        await _socketClient.LoginAsync(TokenType.Bot, token);
+        await _socketClient.StartAsync();
+
+        await Task.Delay(-1); 
+    }
+
+    private static Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
     }
 }
