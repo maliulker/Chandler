5467b00811f743a91a1ab954ce9b2473364becf4
maliulker <maliulker2@gmail.com>
2024-08-15 23:32:16 +0300

    add hello command

--- Chandler/Modules/FunModule.cs
+++ Chandler/Modules/FunModule.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading.Tasks;
 using Discord.Interactions;
+using Discord.WebSocket;
 
 
 namespace Chandler.Modules;
@@ -12,4 +13,12 @@ public class FunModule : InteractionModuleBase
     {
         await RespondAsync(input);
     }
+
+    [SlashCommand("hello", "greet the user")]
+    public async Task Hello()
+    {
+        var guildUser = (SocketGuildUser)Context.User;
+        await RespondAsync($"Hello {guildUser.DisplayName}");
+    }
+
 }
